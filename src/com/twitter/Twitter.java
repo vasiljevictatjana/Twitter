3c181a0a3cedccd348698eadc5bd7b86b6124f0e
Tatjana <vasiljevictatjana@yahoo.com>
2015-04-07 17:42:29 +0200

    Naravljena JavaDoc za klasu Twitter i stavljeno da git ignorise doc folder sa javaDoc HTML dokumentacijom

--- src/com/twitter/Twitter.java
+++ src/com/twitter/Twitter.java
@@ -3,13 +3,34 @@ package com.twitter;
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
 
+/**
+ * 
+ * @author Tatjana
+ *
+ */
 public class Twitter {
+	
+	/**
+	 * Lista poruke tipa TwitterPoruka.
+	 */
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
 
+	/**
+	 * Meotda vraca listu poruke.
+	 * 
+	 * @return vraca listu poruke.
+	 */
 	public LinkedList<TwitterPoruka> vratiSvePoruke() {
 		return poruke;
 	}
 
+	/**
+	 * Metoda pravi novu poruku i puni podacima, i 
+	 * poruka se unosi u listu poruke na kraj.
+	 * 
+	 * @param korisnik username korisnika.
+	 * @param poruka poruka.
+	 */
 	public void unesi(String korisnik, String poruka) {
 		// Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
@@ -19,6 +40,14 @@ public class Twitter {
 		poruke.addLast(tp);
 	}
 
+	/**
+	 * Metoda vraca niz poruka koje sadrze tag(dat kao ulazni parametar) 
+	 * i to onoliko poruka koliko je uneto kao parametar maxBroj
+	 *
+	 * @param maxBroj maksimalan broj poruka koji se moze uneti
+	 * @param tag string koji sadrzi poruka.
+	 * @return vraca se niz poruka sa tagom.
+	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 		if (tag == null || tag.isEmpty())
 			throw new RuntimeException("Morate uneti tag");
